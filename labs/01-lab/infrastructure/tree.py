3f7dd6c52b9d696b0a2bce9cf0635de1d701d70e
VedikaSrivastava <83489280+VedikaSrivastava@users.noreply.github.com>
2022-09-15 13:08:33 -0400

    Update tree.py

--- labs/01-lab/infrastructure/tree.py
+++ labs/01-lab/infrastructure/tree.py
@@ -1,3 +1,19 @@
+"""
+Author: Vedika Srivastava
+Uni Id: U26624361
+"""
+
+
 def draw_tree():
-    print("tree not found")
+    for u in range(0,4):
+        print("     /   \\")
+        print("    /     \\")
+        print("   /       \\")
+        print("  /         \\")
+        print("  ___________")
+    for i in range(0,5):print("\t|     |")
+    print("\t|_____|")
     return
+
+
+draw_tree()
