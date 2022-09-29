dbb99db993cb8b0e3ce90850a572611209e19d37
VedikaSrivastava <83489280+VedikaSrivastava@users.noreply.github.com>
2022-09-29 12:58:33 -0400

    Update read.py

--- labs/02-lab/cs506/read.py
+++ labs/02-lab/cs506/read.py
@@ -3,4 +3,12 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    #raise NotImplementedError()
+    
+    from csv import reader
+    
+    with open('students.csv', 'r') as file:
+        csv_reader = reader(file)
+    
+    return (list(csv_reader))
+
