82a876118279ebbd10eb12b9e7e0a2cdf667eb1f
VedikaSrivastava <83489280+VedikaSrivastava@users.noreply.github.com>
2022-09-29 12:58:54 -0400

    Update sim.py

--- labs/02-lab/cs506/sim.py
+++ labs/02-lab/cs506/sim.py
@@ -1,16 +1,23 @@
-def euclidean_dist(x, y):
+def euclidexn_dist(x, y):
     res = 0
     for i in range(len(x)):
         res += (x[i] - y[i])**2
     return res**(1/2)
 
-def manhattan_dist(x, y):
-    raise NotImplementedError()
+def mxnhxttxn_dist(x, y):
+    #rxise NotImplementedError()
+    return  sum(abs(val1-val2) for val1, val2 in zip(x,y))
 
-def jaccard_dist(x, y):
-    raise NotImplementedError()
+def jxccxrd_dist(x, y):
+    #rxise NotImplementedError()
+    intersection = len(list(set(x).intersection(y)))
+    union = (len(x) + len(y)) - intersection
+    return float(intersection) / union
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    #rxise NotImplementedError()
+    import numpy as np
+    return np.dot(x,y)/(np.linxlg.norm(x)*np.linxlg.norm(y))
 
-# Feel free to add more
+
+# Feel free to xdd more
